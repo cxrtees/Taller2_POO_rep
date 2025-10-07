17bc71459de89f883976e4210aef95ad588a106e
cxrtees <benjamincortes2889@gmail.com>
2025-10-07 13:09:20 -0300

    testeo leer txt
    se crea funciones para comprobar si se leen bien los txt

--- Taller2_POO/src/Taller2_POO/Main.java
+++ Taller2_POO/src/Taller2_POO/Main.java
@@ -1,11 +1,61 @@
 package Taller2_POO;
 
-public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class Main {
+	
+	private static Scanner s = new Scanner(System.in);
+	
+	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("asdadasdsad");
 		System.out.println("ksdjaksdjas");
+		
+		leerPcs();
+		leerPuertos();
+		leerUsuarios();
+		leerVul();
+	}
 
+	private static void leerVul() throws FileNotFoundException{
+		File arch = new File("vulnerabilidades.txt");
+		s = new Scanner(arch);
+		while (s.hasNextLine()) {
+			String linea = s.nextLine();
+			String[] partes = linea.split("|");
+			System.out.println(linea); //testeo	
+		}
+	}
+
+	private static void leerUsuarios() throws FileNotFoundException{
+		File arch = new File("usuarios.txt");
+		s = new Scanner(arch);
+		while (s.hasNextLine()) {
+			String linea = s.nextLine();
+			String[] partes = linea.split(";");
+			System.out.println(linea); //testeo	
+		}
+	}
+
+	private static void leerPuertos() throws FileNotFoundException{
+		File arch = new File("puertos.txt");
+		s = new Scanner(arch);
+		while (s.hasNextLine()) {
+			String linea = s.nextLine();
+			String[] partes = linea.split("|");
+			System.out.println(linea); //testeo		
+		}
+	}
+
+	private static void leerPcs() throws FileNotFoundException {
+		File arch = new File("pcs.txt");
+		s = new Scanner(arch);
+		while (s.hasNextLine()) {
+			String linea = s.nextLine();
+			String[] partes = linea.split("|");
+			System.out.println(linea); //testeo		
+		}	
 	}
 
 }
