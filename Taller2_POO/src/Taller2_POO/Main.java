7178f93508ceceeac20732893f5a5d7e2b6d4d14
cxrtees <benjamincortes2889@gmail.com>
2025-10-06 21:17:21 -0300

    xd
    xd

--- Taller2_POO/src/Taller2_POO/Main.java
+++ Taller2_POO/src/Taller2_POO/Main.java
@@ -3,7 +3,8 @@ package Taller2_POO;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("asdadasdsad");
+		System.out.println("ksdjaksdjas");
 
 	}
 
