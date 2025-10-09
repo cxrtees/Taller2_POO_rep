99c8aff5a3b5d3646ff98e17e30256cb238566de
cxrtees <benjamincortes2889@gmail.com>
2025-10-08 22:50:00 -0300

    Update PC.java
    gets y toString

--- Taller2_POO/src/Taller2_POO/PC.java
+++ Taller2_POO/src/Taller2_POO/PC.java
@@ -1,5 +1,31 @@
 package Taller2_POO;
 
 public class PC {
+	private String id;
+	private String ip;
+	private String so;
 
+	public PC(String id, String ip, String so) {
+		this.id = id;
+		this.ip = ip;
+		this.so = so;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public String getSo() {
+		return so;
+	}
+
+	@Override
+	public String toString() {
+		return "PC [id=" + id + ", ip=" + ip + ", so=" + so + "]";
+	}	
+	
 }
