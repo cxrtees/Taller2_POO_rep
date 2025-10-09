502a274199b68c3cce0a4fd45183d595f289574c
cxrtees <benjamincortes2889@gmail.com>
2025-10-09 20:02:51 -0300

    Update Puerto.java

--- Taller2_POO/src/Taller2_POO/Puerto.java
+++ Taller2_POO/src/Taller2_POO/Puerto.java
@@ -1,5 +1,53 @@
 package Taller2_POO;
 
-public class Puerto {
+import java.util.ArrayList;
 
+public class Puerto {
+	private int numero;
+	private String estado;
+	private PC pcAsociado;
+	private ArrayList<Vulnerabilidad> vulnerabilidades;
+	
+	public Puerto(int numero, String estado, PC pcAsociado)
+	{
+		this.numero = numero;
+		this.estado = estado;
+		this.pcAsociado = pcAsociado;
+		this.vulnerabilidades = new ArrayList<>();
+	}
+	
+	public int getNumero()
+	{
+		return numero;
+	}
+	
+	public String getEstado()
+	{
+		return estado;
+	}
+	
+	public PC getPcAsociado()
+	{
+		return pcAsociado;
+	}
+	
+	public ArrayList<Vulnerabilidad> getVulnerabilidades()
+	{
+		return vulnerabilidades;
+	}
+	
+	public boolean estaAbierto()
+	{
+		return "Abierto".equalsIgnoreCase(estado);
+	}
+	
+	public void agregarVulnerabilidad(Vulnerabilidad vulnerabilidad)
+	{
+		vulnerabilidades.add(vulnerabilidad);
+	}
+	
+	
+	
 }
+
+
