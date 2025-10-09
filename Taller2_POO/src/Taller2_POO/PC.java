e926759c464dbc2187bd53bd6e7ed510ee582761
cxrtees <benjamincortes2889@gmail.com>
2025-10-09 19:57:40 -0300

    funcion obtener claseIP

--- Taller2_POO/src/Taller2_POO/PC.java
+++ Taller2_POO/src/Taller2_POO/PC.java
@@ -27,5 +27,30 @@ public class PC {
 	public String toString() {
 		return "PC [id=" + id + ", ip=" + ip + ", so=" + so + "]";
 	}	
-	
+	public String obtenerClaseIp()
+	{
+		try {
+			String[] partes = ip.split("\\.");
+			int primerOcteto = Integer.parseInt(partes[0]); //primero de los 4 digitos de la direccion ip
+			
+			if(primerOcteto >= 0 && primerOcteto <= 127) //si el octeto esta entre 0 y 127 es clase A
+			{
+				return "Clase A";
+				
+			} else if (primerOcteto >= 128 && primerOcteto <= 191) //si esta entre 128 y 191 es clase B
+			{
+				return "Clase B";
+				
+			} else if (primerOcteto >= 192 && primerOcteto <= 223) //si esta entre 192 y 223 es clase C
+			{
+				return "Clase C";
+				
+			} else 
+			{
+				return "Otra";  
+			}
+		} catch (Exception e) {
+			return "Error, clase desconocida";
+		}
+	}
 }
