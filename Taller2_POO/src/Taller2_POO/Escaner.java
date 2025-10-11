e4fd21b40e43344ef87a38aefd8b9e1bab696a1a
Catalina Rojas <cataa.rojas100@gmail.com>
2025-10-10 23:21:57 -0300

    Update clase Escaner
    se implementa la función de generar reporte para crear el formato que se guardara en reporte.

--- Taller2_POO/src/Taller2_POO/Escaner.java
+++ Taller2_POO/src/Taller2_POO/Escaner.java
@@ -27,5 +27,32 @@ public class Escaner {
 	{
 		return fecha;
 	}
+	
+	public String generarReporte()
+	{
+		String reporte = "";
+		reporte += "=== Reporte de Escaneo ===\n";
+		reporte += "PC: " + pc.getId() + "\n";
+		reporte += "IP: " + pc.getIp() + "\n";
+		reporte += "So: " + pc.getSo() + "\n";
+		reporte += "Usuario: " + usuario.getNombre() + "\n";
+		reporte += "Nivel de Riesgo: " + pc.calcularNivelRiesgo() + "\n";
+		reporte += "Fecha: " + fecha.toString() + "\n";
+		reporte += "Puertos abiertos:\n";
+		
+		for (Puerto puerto : pc.getPuertos())
+		{
+			if (puerto.estaAbierto())
+			{
+				reporte += " - Puerto " + puerto.getNumero() + "\n";
+				for (Vulnerabilidad vulnerabilidad : puerto.getVulnerabilidades())
+				{
+					reporte += "--> * " + vulnerabilidad.getNombre() + " --> * " + vulnerabilidad.getDescripcion() + "\n"; 	
+				}
+			}
+		}
+		
+		return reporte;
+	}
 
 }
